71e214254ef394af8aa2dec6d2264c2505196250
Danone <mr.super.ko@inbox.ru>
2023-07-03 18:10:17 +0600

    added new field (type) in Marks model

--- main/models.py
+++ main/models.py
@@ -23,6 +23,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Marks(models.Model):
     title = models.CharField(max_length=255, blank=True)
+    type = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     xpos = models.FloatField()
