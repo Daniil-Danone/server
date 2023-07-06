4b5c22aacc9908c3dcfd8eb3b1792110c8165636
Danone <mr.super.ko@inbox.ru>
2023-07-07 03:24:11 +0600

    updated db

--- main/models.py
+++ main/models.py
@@ -9,6 +9,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     surname = models.CharField(max_length=100, blank=True, null=True)
     togo_events = models.TextField(blank=True)
     liked_events = models.TextField(blank=True)
+    age = models.CharField(max_length=255, blank=True, default='Не указано')
+    area = models.CharField(max_length=255, blank=True, default='Не указано')
+    hobyies = models.TextField(blank=True, default='Не указаны')
+    links = models.CharField(max_length=255, blank=True, default='Не указано')
+    about = models.CharField(max_length=255, blank=True, default='Не указано')
     created_at = models.DateTimeField(auto_now_add=True)
     is_superuser = models.BooleanField(blank=True, default=False)
     is_staff = models.BooleanField(blank=True, default=False)
@@ -17,7 +22,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ("name", "surname", "togo_events", "liked_events", "created_at")
+    REQUIRED_FIELDS = ("name", "surname", "togo_events", "liked_events", "age",
+                       "area", "hobyies", "links", "about", "created_at")
 
     def __str__(self):
         return f'UserID {self.id}: {self.name} {self.surname} | {self.email}'
@@ -25,7 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Marks(models.Model):
     title = models.CharField(max_length=255, blank=True)
-    type = models.CharField(max_length=255, blank=True)
+    type = models.CharField(max_length=255, blank=True, default='Не указано')
     description = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     xpos = models.FloatField()
