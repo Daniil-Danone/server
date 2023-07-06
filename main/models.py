9240b2018c63c7e751d7b0c5289fc8e074d87343
Danone <mr.super.ko@inbox.ru>
2023-07-06 18:33:27 +0600

    updated db

--- main/models.py
+++ main/models.py
@@ -8,6 +8,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=100, blank=True, null=True)
     surname = models.CharField(max_length=100, blank=True, null=True)
     togo_events = models.TextField(blank=True)
+    liked_events = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_superuser = models.BooleanField(blank=True, default=False)
     is_staff = models.BooleanField(blank=True, default=False)
@@ -16,7 +17,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ("name", "surname", "togo_events", "created_at")
+    REQUIRED_FIELDS = ("name", "surname", "togo_events", "liked_events", "created_at")
 
     def __str__(self):
         return f'UserID {self.id}: {self.name} {self.surname} | {self.email}'
